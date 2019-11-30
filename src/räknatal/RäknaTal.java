29b3575e58193eb23d2769ace2365bb898b762d7
Harkanwal Singh <hankanallt@gmail.com>
2019-11-30 10:53:30 +0100

    First Commit

---
+++ src/räknatal/RäknaTal.java
@@ -0,0 +1,27 @@
+
+package räknatal;
+
+
+public class RäknaTal {
+
+    
+    public static void main(String[] args) {
+        int i = 44;
+        int j = 3;
+        String name = "Harru";
+        String greet = "Hello";
+        System.out.println(greet + "" + name + "!");
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(greet.length());
+        
+        int number = i *j + 100;
+        System.out.println(number);
+        
+        double d = 45.99;
+        double decimalNum = d / 2.23;
+        System.out.println(decimalNum);
+        System.out.println(d * decimalNum);
+    }
+    
+}
